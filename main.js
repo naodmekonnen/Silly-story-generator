4b842c007b97469c2329a8c4409340c3ff2aa207
naodmekonnen <naod.mekonnen@gmail.com>
2022-09-08 23:57:30 +0000

    replaced strings

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ function randomValueFromArray(array){
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-const insertX = "Willy the Goblin Big Daddy Father Christmas";
+const insertX = ["Willy the Goblin Big Daddy Father Christmas"];
 
-const insertY = "the soup kitchen Disneyland the White House";
+const insertY = ["the soup kitchen Disneyland the White House"];
 
-const insertZ = "spontaneously combusted melted into a puddle on the sidewalk turned into a slug and crawled away";
+const insertZ = ["spontaneously combusted melted into a puddle on the sidewalk turned into a slug and crawled away"];
 
 
 randomize.addEventListener('click', result);
@@ -38,10 +38,12 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300 * 0.0714286) + 'stone';
     const temperature =  Math.round((94-32) * (5/9)) + 'centigrade';
+    const newStory = newStory.replace('94 farenheit', temperature);
+    const newStory = newStory.replace('300 pounds', weight);
 
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
